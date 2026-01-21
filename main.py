c3bbd16c367b078af5c1ab83ad94ce76dbceb507
Nate Richard <nrichard@jpl.nasa.gov>
2026-01-21 15:56:46 -0800

    Update main to have admin record example

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from bespokebpv7.bpv7 import BPv7
 from bespokebpv7.utils import parse_eid_string
 
 
-def test():
-    """Test function to verify processing"""
+def test() -> None:
+    """Test function to verify processing."""
     test_bundle = "9f88071844008202820301820100820100821b000000b5998c982b011a000493e08506021000458202820200850704010042183485010101004454455354ff"
     hex_bundle = binascii.unhexlify(test_bundle)
     a = BPv7(hex_bundle, debug=True)
@@ -17,8 +17,8 @@ def test():
     print(f"Input equals output: {b == test_bundle}")
 
 
-def create_new():
-    """Example to create new bundle"""
+def create_new() -> None:
+    """Create new bundle example."""
     payload = cbor2.dumps("Hello!")
     x = BPv7()
     x.add_payload_block(payload)
@@ -35,8 +35,8 @@ def create_new():
     print(bytes(x).hex())
 
 
-def modify():
-    """Example on modifying bundle"""
+def modify() -> None:
+    """Modify bundle example."""
     y = "9f88070000820282030182028201018202820100821b000000bb0e20b4ea001a000927c08508020100410086010100014d48656c6c6f2c20576f726c64214254b3ff"
     z = BPv7(binascii.unhexlify(y))
     payload = cbor2.dumps("Hello!")
@@ -53,6 +53,16 @@ def modify():
     print(bytes(z))
 
 
+def admin_record_test() -> None:
+    """Admin record processing test."""
+    x = "9f8907184601820282020282028202008202820200821b000000bf77e7c26901192710426a1285060210004582028202008518c103010045840001000085070401004102860101010158268201848481f482f51b000000bf77e7c26981f481f4008202820201821b000000bf77e7c2680042b309ff"
+    z = BPv7(binascii.unhexlify(x))
+    print(z)
+    print(bytes(z).hex())
+    admin = z.get_block_by_type(BlockType.PAYLOAD_BLOCK)
+    print(admin)
+
+
 if __name__ == "__main__":
     print("Verify functionality:")
     test()
@@ -62,3 +72,6 @@ if __name__ == "__main__":
 
     print("\n\nModify existing bundle:")
     modify()
+
+    print("\n\nAdmin record test:")
+    admin_record_test()
