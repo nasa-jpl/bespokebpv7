dcb3c44c0436bf442f3a4fe253b52d27cf470338
Nate Richard <nrichard@jpl.nasa.gov>
2026-01-28 10:24:52 -0800

    Add PCAP parsing example
    Also, use gitattributes to signify pcaps are binary

---
+++ examples/README.md
@@ -0,0 +1,43 @@
+# Examples
+
+This folder contains example scenarios to demostrate bespokebpv7 in use.
+
+## Process a PCAP file
+
+Wireshark has great capability to dissect bundles, but when there many bundles to parse
+ or a bundle block does not yet have a Wireshark dissector, *bespokebpv7* offers a
+ method to extract and parse bundles captured in a PCAP file.
+
+*pcap_parse.py* demonstrates how to extract and parse bundles captured in *test.pcap*.
+ The example script takes the path to the PCAP file and it prints the time the bundle
+ was recorded and quick look information on the bundle. This script can handle bundles
+ mixed with non-DTN traffic, but it will only extract bundles sent over UDP. The format
+ of the PCAP file is also important. Wireshark defaults to saving captures as PCAP Next
+ Generation (PCAPNG) files, but the underlying library that supports the parsing,
+ *dpkt*, cannot parse those files, so any captures need to saved as PCAP files.
+
+## Simplify Unit Tests
+
+Some unit tests may just require sending a bundle to verify functionality. Setting up
+ an entire DTN network is tedious and really overkill just to verify a sent bundle is
+ received and processed correctly. *bespokebpv7* can simplify the setup of these sorts
+ of tests by providing the sending portion of the test. Another test may require
+ verification of bundle format. Instead of capturing a command line utility's output
+ and creating a bash function to parse it, *bespokebpv7* can receive a bundle and look
+ at is assorted parameters directly.
+
+## Support Verification and Validation
+
+Most unit and regression tests look for positive success, will the code do what the
+ developer says it does. A mission taking an implementation will also want to look for
+ a negative success, will the code handle bad data gracefully. This is usually done as
+ part of their verification and validation phase and *bespokebpv7* is a good tool to
+ have during those tests. It offers the ability to set bundle parameters that are not
+ conformant to RFC 9171 or are even malformed, so the implementation can be tested to
+ fail gracefully in off-nominal scenarios.
+
+## Simulate Man in the Middle Attack
+
+BPSec offers a method to prevent man-in-the-middle (MITM) attacks, but at least in ION,
+ there is no test to verify this is handled properly. *bespokebpv7* allows taking in a
+ bundle, modifying it, and sending on to its destination, simulating a MITM attack.
---
+++ examples/pcap_parse/pcap_parse.py
@@ -0,0 +1,109 @@
+"""------------------------------------
+     JET PROPULSION LABORATORY
+------------------------------------
+         ___  _______  ___
+        |   ||       ||   |
+        |   ||    _  ||   |
+        |   ||   |_| ||   |
+     ___|   ||    ___||   |___
+    |       ||   |    |       |
+    |_______||___|    |_______|
+
+------------------------------------
+ CALIFORNIA INSTITUTE OF TECHNOLOGY
+------------------------------------
+
+*****************************************************************************
+ Title: Bundle Protocol Security Classes
+ Author: Nate Richard
+ Modified: 01/28/2026
+ Company: JPL
+ Date:   01/27/2026
+
+ File: bpsec
+ Description:
+           Classes that help create and process BPSec extension blocks
+           Python 3.12.11
+
+Copyright 2025, by the California Institute of Technology. United States
+Government sponsorship acknowledged. Any rights or license to commercial use
+must be negotiated with the Office of Technology Transfer at the California
+Institute of Technology.
+
+This software may be subject to U.S. export control laws and regulations. By
+accepting this software, the user agrees to comply with all applicable U.S.
+export laws and regulations. The user has the responsibility to obtain export
+licenses, or other export authority as may be required before exporting the
+software to foreign countries or providing access to foreign persons.
+*****************************************************************************
+"""
+
+import argparse
+from collections import defaultdict
+from datetime import datetime, timezone
+from pathlib import Path
+
+import dpkt  # type: ignore[import-untyped]
+
+from bespokebpv7.bpv7 import BPv7  # type: ignore[import-untyped]
+
+
+def bp_parse(ts: float, buff: bytes, record: dict[float, list[BPv7]]) -> None:
+    """Extract Bundle data from PCAP data.
+
+    Args:
+        ts: timestamp from PCAP
+        buff: Ethernet frame as bytes
+        record: Append decoded bundle to list
+
+    """
+    try:
+        ip_pkt = dpkt.ethernet.Ethernet(buff).ip  # pyright: ignore[reportAttributeAccessIssue] pylint: disable=E1101
+        data = ip_pkt.udp.data
+    except AttributeError:
+        # pass on non-UDP packets
+        return
+    try:
+        decoded = BPv7(data)
+    except TypeError:
+        # CBOR will likely be able to decode an int from non-bundle UDP data and since
+        # bundles will decode as a list, it'll raise a TypeError when trying to access
+        # non-existing elements the decoded data.
+        return
+    record[ts].append(decoded)
+
+
+def pcap_extract(pcapfile: str) -> dict[float, list[BPv7]]:
+    """Get a dictionary of bundles from a given PCAP file.
+
+    Args:
+        pcapfile: Path object to PCAP file
+
+    Returns:
+        dict with timestamp as key and list of bundles received at given timestamp
+
+    """
+    bundle_dict: dict[float, list[BPv7]] = defaultdict(list)
+    pcappath = Path(pcapfile)
+
+    with pcappath.open("rb") as file:
+        pcapreader = dpkt.pcap.Reader(file)
+        pcapreader.loop(bp_parse, bundle_dict)
+    return bundle_dict
+
+
+if __name__ == "__main__":
+    FSTR = "%Y-%m-%d %H:%M:%S.%f"
+    parser = argparse.ArgumentParser("PCAP file to extract bundles.")
+    parser.add_argument("pcap", help="Path to PCAP file")
+    args = parser.parse_args()
+
+    bundled = pcap_extract(args.pcap)
+
+    if bundled:
+        for tstamp, bundles in bundled.items():
+            timestamp = datetime.fromtimestamp(tstamp, tz=timezone.utc).strftime(FSTR)
+            for bundle in bundles:
+                print(f"Bundle Received: {timestamp}\n{bundle}\n")
+    else:
+        print("No bundles in PCAP.")
