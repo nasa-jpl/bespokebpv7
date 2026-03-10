6c820b9491575a68818f68c0c33b09674aaad64a
Nate Richard <nrichard@jpl.nasa.gov>
2026-03-10 12:22:38 -0700

    Fix bcb tests & formatting

--- src/bespokebpv7/ext_functions.py
+++ src/bespokebpv7/ext_functions.py
@@ -221,11 +221,13 @@ class CustodyTransferExt(CanonicalBlock):
             Converted class to list
 
         """
-        self.data = bundle_converter.dumps([
-            self.sequence_num,
-            self.sequence_id,
-            self._block_src_admin_eid,
-        ])
+        self.data = bundle_converter.dumps(
+            [
+                self.sequence_num,
+                self.sequence_id,
+                self._block_src_admin_eid,
+            ]
+        )
         return super()._unstructure()
 
 
